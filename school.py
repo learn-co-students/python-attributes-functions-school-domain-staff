0e7126054d9cad6319e4f4f71e4eb83e0cfd2d82
github username <github email address>
2018-06-29 10:28:21 -0400

    fixing initialization

--- school.py
+++ school.py
@@ -1,7 +1,7 @@
 class School:
     def __init__(self, name):
         self.name = name
-        self._roster = roster
+        self._roster = {}
 
     def roster(self):
         return self._roster
